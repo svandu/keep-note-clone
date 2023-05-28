dcc153116d101d6732f7dea63b098dce1d67e0ff
svandu <svandu08@gmail.com>
2023-05-28 18:44:12 +0530

    minor changes

--- src/Components/HomePage/index.jsx
+++ src/Components/HomePage/index.jsx
@@ -1,20 +1,12 @@
-import MenuIcon from "@mui/icons-material/Menu";
-import Logo from "../../assets/Images/Logo.png";
-
+import NavBar from "../NavBar";
+import Header from "../Header";
 import "./homepage.scss";
 
 export default function HomePage() {
   return (
     <div className="header-bar-container">
-      <div className="header-part-one">
-        <MenuIcon />
-        <img src={Logo} alt="Logo" className="logo-img" />
-        <p className="logo-name">keep</p>
-      </div>
-      <div className="header-part-two">
-        <input type="text" className="search-bar" />
-      </div>
-      <hr />
+        <NavBar />
+        <Header />
     </div>
   );
 }
